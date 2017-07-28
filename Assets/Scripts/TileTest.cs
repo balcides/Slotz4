68fe54c1d4df0a4049cc1098344c0d21a969cb11
gabe <balcides@gmail.com>
2017-07-27 19:35:42 -0700

    First cycle working
    First cycle working (basic)

--- Assets/Scripts/TileTest.cs
+++ Assets/Scripts/TileTest.cs
@@ -17,20 +17,45 @@ public class TileTest : MonoBehaviour {
 	public Transform TileMasterParent;
 	public int TileXCount;
 	public int TileYCount;
+	Vector3 BottomLine;
+	float BottomLineOffset;
+	float EndTileOffset;
 
 	public bool EnableTileCloneTest;
 	public bool EnableAnimateRowTest;
+
+	public int Unit;
+	public int UnitSpeed;
+
+	//internal
+	int ChildCount;
+	int LastTileCount;
+	Transform EndTile;
+	Transform FirstRow;
+	Vector3 RowTop;
 
 	void Awake(){
 		TileParent = new GameObject("Example GO").transform;
 		TileMasterParent = new GameObject("Example GO Master").transform;
 		TileXCount = 6;
 		TileYCount = 8;
+
+		//Unit
+		Unit = 2000; 
+		UnitSpeed = 1;
+
+		ChildCount = 0;
 	}
 
 	// Use this for initialization
 	void Start () {
 		if(EnableTileCloneTest){	TileCloneTest();	}
+
+		BottomLine = new Vector3(TileParent.position.x,TileParent.position.y - (Unit * 2), TileParent.position.z);
+
+		FirstRow = TileParent; //easier to rememeber
+		LastTileCount = FirstRow.transform.childCount;
+		RowTop = FirstRow.transform.GetChild(LastTileCount - 1).position;
 	}
 	
 	// Update is called once per frame
@@ -56,28 +81,36 @@ public class TileTest : MonoBehaviour {
 	}
 		
 	public void AnimateRowTest(){
-		//take the FIRST ROW and MOVE IT down by 1 UNIT (whatever that is)
-		Transform FirstRow = TileParent;			//FirstRow
 
-		//Unit
-		int Unit = 4000;
-		int UnitSpeed = 10;
+		//take the FIRST ROW 
+		FirstRow = TileParent;
 
-		//MoveDown
-		FirstRow.Translate(0, (Time.deltaTime * (-2000) * UnitSpeed), 0);
-
-		//Target is one unit down from current pos
-		Vector3 Target = new Vector3(FirstRow.position.x,FirstRow.position.y - Unit, FirstRow.position.z);
-		float Step = UnitSpeed * Time.deltaTime;
-		FirstRow.position = Vector3.MoveTowards(FirstRow.position, Target, Step);
+		//MOVE IT down by 1 UNIT (whatever that is (Target is one unit down from current pos)
+		float Step = UnitSpeed * Time.deltaTime * (Unit * 2);
+		FirstRow.position = Vector3.MoveTowards(FirstRow.position, BottomLine, Step);
 
 		//and then take the TILE AT THE END and MOVE IT to the TOP OF THE ROW
-		//EndTile
-		//MoveIt
-		//RowTop
-	}
+		EndTile = FirstRow.transform.GetChild(ChildCount);
+		EndTile.gameObject.GetComponent<Renderer>().material.color = Color.red;
 
-	public void AnimateTileTest(){
-		
+		//RowTop
+		// top of the row is the last tile position y plus 1 unit, get length of row
+		//RowTop = FirstRow.transform.GetChild(LastTileCount - 1).position;
+		Vector3 RowTopAbove = new Vector3(RowTop.x, RowTop.y + (Unit * 2), RowTop.z);
+
+		BottomLineOffset = BottomLine.y * 0.5f; 
+		EndTileOffset = EndTile.position.y * (ChildCount + 1);
+
+		//When the tiles move down 1 unit
+		if(EndTileOffset == BottomLineOffset){
+			print("=EndTileOffset: " + EndTileOffset);
+			print("=BottomLineOffset: " + BottomLineOffset);
+			EndTile.position = RowTopAbove;
+
+			//Reset
+			if(ChildCount < (LastTileCount - 1)){ 	ChildCount++; 	}
+			EndTile = FirstRow.transform.GetChild(ChildCount);
+			BottomLine = new Vector3(BottomLine.x, BottomLine.y - (Unit * 2), BottomLine.z);
+		}
 	}
 }
