e664081df2e46f45a767917e3d0e7425659953f1
gabe <balcides@gmail.com>
2017-08-01 20:34:22 -0700

    Y rows in transform vars setup
    Y rows in transform vars setup

--- Assets/Scripts/SpinSlotsScript.cs
+++ Assets/Scripts/SpinSlotsScript.cs
@@ -18,8 +18,7 @@ public class SpinSlotsScript : MonoBehaviour {
 	public int TileYCount;
 	public int Unit;
 	public int SpinCountBeforeStop;
-	public bool EnableTileCloneTest;
-	public bool EnableSpinSlotTest;
+	public bool EnableCloneTileTest;
 	public float UnitSpeed;
 	public float UnitSpeedStart;
 
@@ -34,71 +33,94 @@ public class SpinSlotsScript : MonoBehaviour {
 	int LastTileCount;
 	Transform EndTile;
 	Transform FirstRowX;
+	Transform[] XRow;
+	Transform[] YRow;
 	Vector3 RowTop; 
 	Vector3[] TileXPositions;
+	Vector3[] TileYPositions;
+	bool EnableSpinSlotTest;
 	bool disableSpinButton;
 
+	//get an array to store all the y parents
+	//then a series of arrays for the x's
+
 	void Awake(){
-		FirstRowX = new GameObject("Example GO").transform;
-		TileMasterParent = new GameObject("Example GO Master").transform;
 		TileXCount = 6;
 		TileYCount = 8;
 
 		//Unit
 		Unit = 2000; 
-		UnitSpeed = 10;			//10 too fast, 5 too slow
+		UnitSpeed = 10;					//10 too fast, 5 too slow
 		UnitSpeedStart = UnitSpeed;
 
 		ChildCount = 0;
 		SpinCount = 1;
 		SpinCountBeforeStop = 50;
+
+		XRow = new Transform[TileXCount];
+		YRow = new Transform[TileYCount];
+
+		XRow[0] = new GameObject("Example GO").transform;
+		TileMasterParent = new GameObject("Example GO Master").transform;
 	}
 
 	// Use this for initialization
 	void Start () {
-		if(EnableTileCloneTest){	TileCloneTest();	}
-			
-		BottomLine =  new Vector3(FirstRowX.position.x,FirstRowX.position.y - (Unit * 2), FirstRowX.position.z);
-		LastTileCount = FirstRowX.transform.childCount;
-		RowTop = FirstRowX.transform.GetChild(LastTileCount - 1).position;
-		TileParentStart = FirstRowX.position;
+		if(EnableCloneTileTest){	CreateTileGridTransform();	}
+		//Order Matters	
+		BottomLine =  new Vector3(XRow[0].position.x,XRow[0].position.y - (Unit * 2), XRow[0].position.z);
+		LastTileCount = XRow[0].transform.childCount;
+		RowTop = XRow[0].transform.GetChild(LastTileCount - 1).position;
+		TileParentStart = XRow[0].position;
 		TileXPositions = new Vector3[LastTileCount];
+		TileYPositions = new Vector3[TileYCount];
 		GetTilePositions();
 		disableSpinButton = false;
+		//GetTileYPosition();
+		//SetTileYPosition();
+		SetYRowTransform();
+		//GetYRowTransform();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(EnableSpinSlotTest){	SpinSlotTest();	}
+		if(EnableSpinSlotTest){	
+			SpinSlotTest();	
+		}
 	}
 
-	public void TileCloneTest(){
+	//Creates Tile Transform Grid
+	public void CreateTileGridTransform(){
+		Transform XClone;
+		Transform YClone;
+
+		XRow[0].parent = TileMasterParent;
+
 		//Creates the first row X of tiles
 		for (int i = 0; i < TileXCount; i++){
-			Transform Clone;
-			Clone = Instantiate(TilePrefab, new Vector3(2000, 2000 + (4000 * i), 100), Quaternion.Euler(new Vector3(90, 0, 0)));
-			Clone.parent = FirstRowX;
+			XClone = Instantiate(TilePrefab, new Vector3(2000, 2000 + (4000 * i), 100), Quaternion.Euler(new Vector3(90, 0, 0)));
+			XClone.name = XClone.name + i;
+			XClone.parent = XRow[0]; //tiles parented to first row only, itll get duped during Y
 		}
 
 		//dupe to Y Axis
 		for (int i = 1; i < TileYCount; i++){
-			Transform CloneY;
-			CloneY = Instantiate(FirstRowX, new Vector3((4000 * i), 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
-			CloneY.parent = TileMasterParent;
-			FirstRowX.parent = TileMasterParent;
+			YClone = Instantiate(XRow[0], new Vector3((4000 * i), 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+			YClone.name = YClone.name + i;
+			YClone.parent = TileMasterParent;
 		}
 	}
 		
 	public void SpinSlotTest(){
 		//take the FIRST ROW X
-		//FirstRowX = FirstRowX;
+		//XRow[0] = XRow[0];
 
 		//MOVE IT down by 1 UNIT (whatever that is (Target is one unit down from current pos)
 		float Step = UnitSpeed * Time.deltaTime * (Unit * 2);
-		FirstRowX.position = Vector3.MoveTowards(FirstRowX.position, BottomLine, Step);
+		XRow[0].position = Vector3.MoveTowards(XRow[0].position, BottomLine, Step);
 
 		//and then take the TILE AT THE END and MOVE IT to the TOP OF THE ROW
-		EndTile = FirstRowX.transform.GetChild(ChildCount);
+		EndTile = XRow[0].transform.GetChild(ChildCount);
 
 		//RowTop - top of the row is the last tile position y plus 1 unit, get length of row
 		BottomLineOffset = BottomLine.y * 0.5f; //bottom line is tile parent position
@@ -113,7 +135,7 @@ public class SpinSlotsScript : MonoBehaviour {
 			if(ChildCount < (LastTileCount - 1)){ 	ChildCount++; }
 			else{	ChildCount = 0;  }
 			
-			EndTile = FirstRowX.transform.GetChild(ChildCount);
+			EndTile = XRow[0].transform.GetChild(ChildCount);
 			BottomLine = new Vector3(BottomLine.x, BottomLine.y - (Unit * 2), BottomLine.z);
 
 			if(SpinCount >= SpinCountBeforeStop){
@@ -122,43 +144,77 @@ public class SpinSlotsScript : MonoBehaviour {
 				if( UnitSpeed < 2 ){   
 					UnitSpeed = 0;		 
 					EnableSpinSlotTest = false;
-					disableSpinButton = false;}
+					disableSpinButton = false;	}
 				else{ 
 					// spin keeps on spinnin'
-					UnitSpeed = UnitSpeed - (UnitSpeed * 0.25f);}
+					UnitSpeed = UnitSpeed - (UnitSpeed * 0.25f);	}
 			}
-			else{ //nothin
+			else{ //nothin 
 			}
 		}
 	}
 
 	//sets enabled the anime row test and resets the spin
-	public void RunSpinSlot(){
+	public void RunSlotSpin(){
 		if (disableSpinButton){}
 		else{
 			print("SPIN BUTTON PRESSED========================================");
-			EnableSpinSlotTest = true;
-			UnitSpeed = UnitSpeedStart;
-			SpinCount = 1;
-			ChildCount = 0;
-			FirstRowX.position = TileParentStart;
-			SetTilePositions();
-			RowTop = FirstRowX.transform.GetChild(LastTileCount - 1).position;
-			BottomLine = new Vector3(FirstRowX.position.x,FirstRowX.position.y - (Unit * 2), FirstRowX.position.z);
-			disableSpinButton = true;
+			ResetVarsBeforeSpin();
 		}
 	}
 
+	//resets vars before spin
+	void ResetVarsBeforeSpin(){
+		EnableSpinSlotTest = true;
+		UnitSpeed = UnitSpeedStart;
+		SpinCount = 1;
+		ChildCount = 0;
+		XRow[0].position = TileParentStart;
+		SetTilePositions();
+		RowTop = XRow[0].transform.GetChild(LastTileCount - 1).position;
+		BottomLine = new Vector3(XRow[0].position.x,XRow[0].position.y - (Unit * 2), XRow[0].position.z);
+		disableSpinButton = true;
+	}
+
 	//saves the default positions of tiles on start
-	public void GetTilePositions(){
+	void GetTilePositions(){
 		for(int i = 0;  i < (LastTileCount); i++ ){ 
-			Vector3 TileX = FirstRowX.transform.GetChild(i).position;
+			Vector3 TileX = XRow[0].transform.GetChild(i).position;
 			TileXPositions[i] = new Vector3(TileX.x, TileX.y, TileX.z); 
 			}
 	}
-
+		
 	//sets the tile positions back to default
-	public void SetTilePositions(){
-		for(int i = 0;  i < (LastTileCount); i++ ){  FirstRowX.transform.GetChild(i).position = TileXPositions[i]; }
+	void SetTilePositions(){
+		for(int i = 0;  i < (LastTileCount); i++ ){  XRow[0].transform.GetChild(i).position = TileXPositions[i]; }
 	}
+
+	//saves the default positions of tile parents
+	void SetTileYPosition(){
+		for(int i = 0;  i < (TileYCount); i++ ){ 
+			Vector3 TileY = TileMasterParent.transform.GetChild(i).position;
+			TileYPositions[i] = new Vector3(TileY.x, TileY.y, TileY.z); 
+		}
+	}
+
+	//sets tile positions Y to default
+	void GetTileYPosition(){
+		for(int i = 0;  i < (TileYCount); i++ ){ 
+			print(TileYPositions[i]);
+			//TileMasterParent.transform.GetChild(i).position = TileXPositions[i]; 
+		}
+	}
+
+	//get the master parent and Y rows for children
+	void SetYRowTransform(){
+		for(int i = 0;  i < (TileYCount); i++ ){  
+			YRow[i] = TileMasterParent.transform.GetChild(i);
+			YRow[i].name = "TileRow" + i;
+		}
+	}
+
+	void GetYRowTransform(){
+		for(int i = 0;  i < (TileYCount); i++ ){  print(YRow[i]);  }
+	}
+
 }
